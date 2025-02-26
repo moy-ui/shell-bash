f8c5caece8afef3d998d0fa887d2bffd41bab6df
moy-ui <juancor830@gmail.com>
2025-02-25 21:08:09 -0600

    4ejercicio

---
+++ versionligth/4ejercicio.sh
@@ -0,0 +1,11 @@
+#bin/bash
+
+read -p "Ingresa la ruta: " ruta
+echo ''
+echo '***********************************************************************'
+read -p " Se lecciona que quieres hacer con la ruta que haz ingresado
+        a) Comprobar si es un directorio
+        b) Crear el directorio
+        c) Listar su contenido
+      " opcion
+echo '***********************************************************************'
