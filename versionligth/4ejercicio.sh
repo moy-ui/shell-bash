5d7660c07687dc0cc2cc0981cb80a0d3129abb0f
moy-ui <juancor830@gmail.com>
2025-02-26 21:05:41 -0600

    4ejercicio - ejemplo rutas

--- versionligth/4ejercicio.sh
+++ versionligth/4ejercicio.sh
@@ -7,5 +7,38 @@ read -p " Se lecciona que quieres hacer con la ruta que haz ingresado
         a) Comprobar si es un directorio
         b) Crear el directorio
         c) Listar su contenido
-      " opcion
+      
+    Respuesta: " opcion
 echo '***********************************************************************'
+
+case ${opcion} in 
+    a)
+     if [[ -d ${ruta} ]];then
+        echo "La ruta (${ruta}) existe"
+     else
+        echo "La ruta (${ruta}) no existe"
+     fi
+    ;;
+    b)
+      if [[ -d $ruta ]]; then 
+        echo "La ruta (${ruta}) ya existe"
+      else
+        mkdir $ruta
+        if [[ $? -eq 0 ]]; then
+          echo "La ruta (${ruta}) se ha creado con exito !!!"
+        else
+          echo "Algo a salido mal al crear la ruta: ${ruta}"
+        fi
+      fi
+    ;;
+    c)
+      if [[ -d ${ruta} ]]; then
+        ls ${ruta}
+      else
+        echo "No se puede lisar el contenido de \"${ruta}\" porque no existe :("
+      fi
+    ;;
+    *)
+    echo "opcion invalida."
+    ;;
+esac
