3cb4159487994a2e062f0c0498d09af4b0c1ec7b
moy-ui <juancor830@gmail.com>
2025-02-27 20:42:44 -0600

    ejercicio 5 - determinar si el parametro es directorio,fichero o no existe

---
+++ versionligth/5ejercicio.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#programa que determina si el parametro de entrada es una ruta-fichero-noExiste
+
+if [[ $# -gt 0 ]];then
+    if [[ -d $1 ]];then
+        echo "Es una ruta !!!"
+        exit 0
+    fi
+    if [[ -f $1 ]];then 
+        echo "Es un fichero !!"
+        exit 0
+    fi
+else
+    echo 'No ha ingresado ningun parametro :( '
+    exit -1
+fi
